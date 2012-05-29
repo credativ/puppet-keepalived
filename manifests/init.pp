6e1940727550b89ce27feb5d08a2bf17a27052c0
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2012-05-29 10:40:43 +0200

    Add module documentation

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,38 @@
+# = Class: keepalived
+#
+# Module to manage keepalived
+#
+# == Requirements:
+#
+# - This module makes use of the example42 functions in the puppi module
+#   (https://github.com/credativ/puppet-example42lib)
+#
+# == Parameters:
+#
+# [* ensure *]
+#   What state to ensure for the package. Accepts the same values
+#   as the parameter of the same name for a package type.
+#   Default: present
+#   
+# [* ensure_running *]
+#   Weither to ensure running keepalived or not.
+#   Default: running
+#
+# [* ensure_enabled *]
+#   Weither to ensure that keepalived is started on boot or not.
+#   Default: true
+#
+# [* config_source *]
+#   Specify a configuration source for the configuration. If this
+#   is specified it is used instead of a template-generated configuration
+#
+# [* config_template *]
+#   Override the default choice for the configuration template
+#
+# == Author:
+#
+#   Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
+
 class keepalived (
     $ensure             = params_lookup('ensure'),
     $manage_config      = params_lookup('manage_config'),
