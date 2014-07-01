5d7bea4c1d29637d61f4a5135cfa846b853ea48f
Patrick Schoenfeld <patrick.schoenfeld@credativ.de>
2014-07-01 11:32:59 +0200

    Bugfix for commit 0e23baefb9058558910cb9b1d72d55c90696ba2a
    Add a (empty) default for virtual_server_groups, making them optional,
    and  remove a non-working line from keepalived.conf template, which was
    supposed to make virtual_server_groups optional, but made it fail at all.

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,14 +1,15 @@
 # Define defaults for the keepalived module
 class keepalived::params {
     # Module options
-    $ensure             = present
-    $ensure_running     = true
-    $ensure_enabled     = true
-    $manage_config      = true
-    $config_template    = 'keepalived/keepalived.conf.erb'
-    $config_source      = undef
-    $disabled_hosts     = []
-    $notification_email = 'root@localhost'
-    $smtp_server        = '127.0.0.1'
+    $ensure                 = present
+    $ensure_running         = true
+    $ensure_enabled         = true
+    $manage_config          = true
+    $config_template        = 'keepalived/keepalived.conf.erb'
+    $config_source          = undef
+    $disabled_hosts         = []
+    $notification_email     = 'root@localhost'
+    $smtp_server            = '127.0.0.1'
+    $virtual_server_groups = []
 
 }
